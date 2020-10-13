f733946ef27169426fbba4e802e96da2948e13be
imperative-scientist-4600 <nelom3@gmail.com>
2020-10-13 15:03:30 +0000

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,20 +1,20 @@
 class Person
-  attr_accessor :partner, :name
-  
+  attr_accessor :name, :partner
+ 
   def initialize(name)
     @name = name
   end
-
+ 
   def get_married(person)
     self.partner = person
     if person.class != Person
       begin
         raise PartnerError
-      rescue PartnerError => error
-          puts error.message
+      rescue PartnerError => error  
+        puts error.message
       end
     else
-      person.partner = self
+      perosn.partner = self
     end
   end
   
@@ -24,7 +24,7 @@ class Person
     end
   end
 end
-
+ 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
